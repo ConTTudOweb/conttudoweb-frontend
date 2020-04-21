78683a36ae657a511c4f6a375064479bc52eb4dc
Alessandro de Lima Folk <sandrofolk@hotmail.com>
2020-04-21 15:10:37 -0300

    Teste deploy

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-layout column justify-center align-center>
+    Olá!!!
+  </v-layout>
+</template>
+
+<script>
+export default {
+  layout: 'public',
+  auth: 'guest',
+  middleware({ store, redirect }) {
+    if (store.state.subDomain) {
+      return redirect('/dashboard')
+    }
+  }
+}
+</script>
